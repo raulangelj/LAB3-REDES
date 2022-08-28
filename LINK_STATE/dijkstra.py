0e965f85fa9b87740cc95fb9eb30925c5831da59
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-08-28 16:09:31 -0600

    add new dijkstra algorithm with path

--- LINK_STATE/dijkstra.py
+++ LINK_STATE/dijkstra.py
@@ -1,67 +1,94 @@
-# extraido de:https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+#extraido de: https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
 import sys
  
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+        
+    def construct_graph(self, nodes, init_graph):
+        '''
+        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+        
+        graph.update(init_graph)
+        
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+                    
+        return graph
+    
+    def get_nodes(self):
+        "Returns the nodes of the graph."
+        return self.nodes
+    
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+    
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        return self.graph[node1][node2]
 
-class Graph():
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
  
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
+    shortest_path = {}
  
-    def printSolution(self, dist):
-        print("Vertex \tDistance from Source")
-        for node in range(self.V):
-            print(node, "\t", dist[node])
+    # We'll use this dict to save the shortest known path to a node found so far
+    previous_nodes = {}
  
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
-    def minDistance(self, dist, sptSet):
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0   
+    shortest_path[start_node] = 0
+    
+    # The algorithm executes until we visit all nodes
+    while unvisited_nodes:
+        # The code block below finds the node with the lowest score
+        current_min_node = None
+        for node in unvisited_nodes: # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+                
+        # The code block below retrieves the current node's neighbors and updates their distances
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
  
-        # Initialize minimum distance for next node
-        min = sys.maxsize
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+    
+    return previous_nodes, shortest_path
+
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+    
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
  
-        # Search not nearest vertex not in the
-        # shortest path tree
-        for u in range(self.V):
-            if dist[u] < min and sptSet[u] == False:
-                min = dist[u]
-                min_index = u
- 
-        return min_index
- 
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
-    def dijkstra(self, src):
-        path = []
- 
-        dist = [sys.maxsize] * self.V
-        dist[src] = 0
-        sptSet = [False] * self.V
- 
-        for cout in range(self.V):
- 
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # x is always equal to src in first iteration
-            x = self.minDistance(dist, sptSet)
- 
-            # Put the minimum distance vertex in the
-            # shortest path tree
-            sptSet[x] = True
- 
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            shortest = []
-            for y in range(self.V):
-                if self.graph[x][y] > 0 and sptSet[y] == False and \
-                        dist[y] > dist[x] + self.graph[x][y]:
-                    shortest.append((x, y))
-                    dist[y] = dist[x] + self.graph[x][y]
-            path.append(shortest)
-        print('path:', path)
-        self.printSolution(dist)
+    # Add the start node manually
+    path.append(start_node)
+    
+    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+    print(" -> ".join(reversed(path)))
