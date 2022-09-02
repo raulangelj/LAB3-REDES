61b935c017752cfb09a0e6051f25ac11f7a2cd76
Donaldo Sebastian Garcia <54748964+donaldosebas@users.noreply.github.com>
2022-09-01 21:21:09 -0400

    remove unused code

--- LINK_STATE/dijkstra.py
+++ LINK_STATE/dijkstra.py
@@ -1,4 +1,3 @@
-#extraido de: https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
 import sys
  
 class Graph(object):
@@ -7,9 +6,6 @@ class Graph(object):
         self.graph = self.construct_graph(nodes, init_graph)
         
     def construct_graph(self, nodes, init_graph):
-        '''
-        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
-        '''
         graph = {}
         for node in nodes:
             graph[node] = {}
@@ -40,32 +36,25 @@ class Graph(object):
         return self.graph[node1][node2]
 
 def dijkstra_algorithm(graph, start_node):
-    unvisited_nodes = list(graph.get_nodes())
- 
-    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
+    unvisited_nodes = list(graph.get_nodes()) 
     shortest_path = {}
  
-    # We'll use this dict to save the shortest known path to a node found so far
     previous_nodes = {}
- 
-    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
+  
     max_value = sys.maxsize
     for node in unvisited_nodes:
         shortest_path[node] = max_value
-    # However, we initialize the starting node's value with 0   
+      
     shortest_path[start_node] = 0
     
-    # The algorithm executes until we visit all nodes
     while unvisited_nodes:
-        # The code block below finds the node with the lowest score
         current_min_node = None
-        for node in unvisited_nodes: # Iterate over the nodes
+        for node in unvisited_nodes:
             if current_min_node == None:
                 current_min_node = node
             elif shortest_path[node] < shortest_path[current_min_node]:
                 current_min_node = node
                 
-        # The code block below retrieves the current node's neighbors and updates their distances
         neighbors = graph.get_outgoing_edges(current_min_node)
         for neighbor in neighbors:
             tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
@@ -74,7 +63,6 @@ def dijkstra_algorithm(graph, start_node):
                 # We also update the best path to the current node
                 previous_nodes[neighbor] = current_min_node
  
-        # After visiting its neighbors, we mark the node as "visited"
         unvisited_nodes.remove(current_min_node)
     
     return previous_nodes, shortest_path
@@ -87,7 +75,6 @@ def print_result(previous_nodes, shortest_path, start_node, target_node):
         path.append(node)
         node = previous_nodes[node]
 
-    # Add the start node manually
     path.append(start_node)
 
     print(f"We found the following best path with a value of {shortest_path[target_node]}.")
